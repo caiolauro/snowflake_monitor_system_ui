8dfd305096d7d37d7d27d6b6b04670cd29297f1f
clauro <caio.lauro@flixbus.com>
2023-12-05 19:27:23 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const MoistureApp = () => {
+  // Assuming you have an API endpoint to fetch moisture data
+  const apiEndpoint = 'https://random-data-api.com/api/v2/credit_cards';
+
+  const [moistureData, setMoistureData] = useState(null);
+
+  useEffect(() => {
+    // Fetch moisture data from the API
+    const fetchMoistureData = async () => {
+      try {
+        const response = await fetch(apiEndpoint);
+        const data = await response.json();
+        console.log(data)
+        setMoistureData(data.credit_card_number);
+      } catch (error) {
+        console.error('Error fetching moisture data:', error);
+      }
+    };
+
+    fetchMoistureData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="moisture-app">
+      <header>
+        <h1>Moisture Dashboard</h1>
       </header>
+      <main>
+        {moistureData ? (
+          <div className=".moisture-info">
+            <h2>Current Moisture Level</h2>
+            <p>{moistureData}%</p>
+            {/* You can add more details or visualizations here */}
+          </div>
+        ) : (
+          <p>Loading moisture data...</p>
+        )}
+      </main>
+      <footer>
+        <p>© 2023 Your Company</p>
+      </footer>
     </div>
   );
-}
+};
 
-export default App;
+export default MoistureApp;
